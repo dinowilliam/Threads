79c32f1e49e02dea65fc90dfcaaeb653bde0446e
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2021-12-28 19:51:39 -0300

    Changes in the Menu and in the Foder Organization

---
+++ POCThreads/Menu/MainMenu.cs
@@ -0,0 +1,104 @@
+﻿using POCThreads.Part1GettingStarted.POCS;
+using System;
+
+namespace POCThreads.Menu
+{
+    public class MainMenu {
+        public void DrawMainScreen() {
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("|                                                  dinowilliam.com                                                    |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("|                                               Exercises from Threads                                                |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("|                                                                                                                     |");
+            Console.WriteLine("|                                        1 - Part 1 - Getting Started                                                 |");
+            Console.WriteLine("|                                        2 -                                                                          |");
+            Console.WriteLine("|                                        3 -                                                                          |");
+            Console.WriteLine("|                                        4 -                                                                          |");
+            Console.WriteLine("|                                        5 -                                                                          |");
+            Console.WriteLine("|                                        6 -                                                                          |");
+            Console.WriteLine("|                                        7 -                                                                          |");
+            Console.WriteLine("|                                        8 -                                                                          |");
+            Console.WriteLine("|                                        0 - Exit                                                                     |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("| Choose one option to continue...                                                                                    |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+
+            var option = Console.ReadLine();
+
+            ExecuteTheOption(option);
+        }
+
+        public void ExecuteTheOption(string option)
+        {
+
+            if (!String.IsNullOrWhiteSpace(option))
+            {
+
+                int value = Int32.Parse(option);
+
+                switch (value)
+                {
+
+                    case 1:
+                        var part1GettingStartedMenu = new Part1GettingStartedMenu();
+                        part1GettingStartedMenu.DrawMainScreen();
+
+                        DrawMainScreen();
+                        break;
+
+                    case 2:                        
+
+                        DrawMainScreen();
+                        break;
+
+                    case 3:                        
+
+                        DrawMainScreen();
+                        break;
+
+                    case 4:                        
+
+                        DrawMainScreen();
+                        break;
+
+                    case 5:                        
+
+                        DrawMainScreen();
+                        break;
+
+                    case 6:
+
+                        DrawMainScreen();
+                        break;
+
+                    case 7:                        
+
+                        DrawMainScreen();
+                        break;
+
+                    case 8:
+
+                        DrawMainScreen();
+                        break;
+
+                    case 0:
+                        
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        DrawMainScreen();
+                        break;
+                }
+            }
+            else
+            {
+                DrawMainScreen();
+            }
+        }
+    }
+}
--- POCThreads/Program.cs
+++ POCThreads/Program.cs
@@ -1,131 +1,20 @@
-﻿using POCThreads.Part1GettingStarted.POCS;
+﻿using POCThreads.Menu;
+using POCThreads.Part1GettingStarted.POCS;
 using System;
 
 namespace POCThreads
 {
-    class Program
-    {
+    class Program {
+        
         static void Main(string[] args) {
-            DrawMainScreen();
+            MainScreen();
         }
 
-        public static void DrawMainScreen()
-        {
-
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("|*********************************************************************************************************************|");
-            Console.WriteLine("|                                                  dinowilliam.com                                                    |");
-            Console.WriteLine("|*********************************************************************************************************************|");
-            Console.WriteLine("|                                               Exercises from Threads                                                |");
-            Console.WriteLine("|*********************************************************************************************************************|");
-            Console.WriteLine("|                                Getting Started                                                                      |");
-            Console.WriteLine("|                                        1 - Thread Test 1                                                            |");
-            Console.WriteLine("|                                        2 - Thread Test 2                                                            |");
-            Console.WriteLine("|                                        3 - Thread Test 3                                                            |");
-            Console.WriteLine("|                                        4 - Thread Test 4                                                            |");
-            Console.WriteLine("|                                        5 - Thread Test 5                                                            |");
-            Console.WriteLine("|                                        6 - Thread Safe                                                              |");
-            Console.WriteLine("|                                        7 - Thread Join                                                              |");
-            Console.WriteLine("|                                        8 - Thread Sleep                                                             |");
-            Console.WriteLine("|                                        0 - Exit                                                                     |");
-            Console.WriteLine("|*********************************************************************************************************************|");
-            Console.WriteLine("| Choose one option to continue...                                                                                    |");
-            Console.WriteLine("|*********************************************************************************************************************|");
-
-            var option = Console.ReadLine();
-
-            ExecuteTheOption(option);
+        static void MainScreen() {
+            var mainMenu = new MainMenu();
+            mainMenu.DrawMainScreen();
         }
-
-        public static void ExecuteTheOption(string option)
-        {
-
-            if (!String.IsNullOrWhiteSpace(option))
-            {
-
-                int value = Int32.Parse(option);
-
-                switch (value)
-                {
-
-                    case 1:
-                        var pocThreadTest1 = new PocThreadTest1();
-                        pocThreadTest1.StartThreadTest1();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 2:
-
-                        var pocThreadTest2 = new PocThreadTest2();
-                        pocThreadTest2.StartThreadTest2();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 3:
-
-                        var pocThreadTest3 = new PocThreadTest3();
-                        pocThreadTest3.StartThreadTest3();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 4:
-
-                        var pocThreadTest4 = new PocThreadTest4();
-                        pocThreadTest4.StartThreadTest4();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 5:
-
-                        var pocThreadTest5 = new PocThreadTest5();
-                        pocThreadTest5.StartThreadTest5();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 6:
-
-                        var pocThreadSafe = new PocThreadSafe();
-                        pocThreadSafe.StartThreadSafe();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 7:
-
-                        var pocThreadJoin = new PocThreadJoin();
-                        pocThreadJoin.StartThreadJoin();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 8:
-
-                        var pocThreadSleep = new PocThreadSleep();
-                        pocThreadSleep.StartThreadSleep();
-
-                        DrawMainScreen();
-                        break;
-
-                    case 0:
-                        Environment.Exit(0);
-                        break;
-
-                    default:
-                        DrawMainScreen();
-                        break;
-                }
-            }
-            else
-            {
-                DrawMainScreen();
-            }
-        }
+        
 
     }
 }
